2ae2f17f81297bcacde594674af9830aeb20f211
DeCrys <hry1504@gmail.com>
2025-08-26 07:15:53 +0200

    resizable-navbar.tsx

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -94,7 +94,7 @@ export const NavbarLogo = () => {
     }
     
     try {
-      const res = await fetch("https://craftmaga.cz/api/login", {
+      const res = await fetch("https://crafmaga-web-production.up.railway.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ nick: inputNick }),
@@ -174,3 +174,4 @@ export const NavbarLogo = () => {
   );
 
 };
+
