33e38b3e117e42c763ebf2771104194799ca4807
DeCrys <hry1504@gmail.com>
2025-08-29 12:52:26 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
