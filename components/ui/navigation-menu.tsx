fe8f8baa878b2c69d3b0e2e3a69dd0f23129180f
DeCrys <hry1504@gmail.com>
2025-09-06 10:44:27 +0200

    Delete components directory

--- components/ui/navigation-menu.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
-import { ChevronDown } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { navigationMenuTriggerStyle } from "./navigationMenuUtils";
-
-// ...komponenty jako předtím, jen bez definice navigationMenuTriggerStyle
