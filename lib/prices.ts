4647013beb407dee41a7786ea5b6bbc0dc72bcbb
DeCrys <hry1504@gmail.com>
2025-09-06 14:02:35 +0200

    Update prices.ts

--- lib/prices.ts
+++ lib/prices.ts
@@ -4,10 +4,10 @@ export const PRICE_MAP: Record<
   string,
   { name: string; amountCZK: number; description?: string; rank: string }
 > = {
-  '6414884': { name: 'Knight Rank',  amountCZK: 70,  description: 'VIP Knight Rank',  rank: 'knight' },
-  '6824767': { name: 'Lord Rank',    amountCZK: 90,  description: 'VIP Lord Rank',    rank: 'lord' },
-  '6824769': { name: 'Paladin Rank', amountCZK: 110, description: 'VIP Paladin Rank', rank: 'paladin' },
-  '6824773': { name: 'Duke Rank',    amountCZK: 130, description: 'VIP Duke Rank',    rank: 'duke' },
-  '6824777': { name: 'King Rank',    amountCZK: 170, description: 'VIP King Rank',    rank: 'king' },
+  '6414884': { name: 'VIP Rank',  amountCZK: 70,  description: 'VIP Knight Rank',  rank: 'vip' },
+  '6824767': { name: 'LEGEND Rank',    amountCZK: 90,  description: 'VIP Lord Rank',    rank: 'legend' },
+  '6824769': { name: 'ULTRA Rank', amountCZK: 110, description: 'VIP Paladin Rank', rank: 'ultra' },
+  '6824773': { name: 'GOD Rank',    amountCZK: 130, description: 'VIP Duke Rank',    rank: 'god' },
+  '6824777': { name: 'IMMORTAL Rank',    amountCZK: 170, description: 'VIP King Rank',    rank: 'immortal' },
 }
 
